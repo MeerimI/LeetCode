c39b27b44831b927ce5c8787ff2a8d5e2aa346de
mery <merryoff@gmail.com>
2021-02-24 21:57:31 +0600

    added another solution to the problem

--- TargetArrayInTheGivenOrder.py
+++ TargetArrayInTheGivenOrder.py
@@ -1,6 +1,6 @@
 """
 Runtime: 24 ms, faster than 44.53% of Python online submissions for Create Target Array in the Given Order.
-Memory Usage: 13.5 MB, less than 15.00% of Python online submissions for Create Target Array in the Given Order.
+Memory Usage: 13.4 MB, less than 70.55% of Python online submissions for Create Target Array in the Given Order.
 
 """
 
@@ -8,6 +8,5 @@ class Solution(object):
     def createTargetArray(self, nums, index):
                      
         result = []
-        for i in range(len(nums)):
-            result.insert(index[i], nums[i])
+        for i in range(len(nums)): result.insert(index[i], nums[i])
         return result
