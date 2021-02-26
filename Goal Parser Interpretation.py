13bd36f76261ffd0d3a3a75f848466b3f9d16f64
mery <merryoff@gmail.com>
2021-02-26 20:29:17 +0600

    1678. Goal Parser Interpretation

--- Goal Parser Interpretation.py
+++ Goal Parser Interpretation.py
@@ -3,6 +3,7 @@ Runtime: 16 ms, faster than 76.36% of Python online submissions for Goal Parser 
 Memory Usage: 13.4 MB, less than 63.76% of Python online submissions for Goal Parser Interpretation.
 
 """
+
 class Solution(object):
     def interpret(self, command):       
         command = command.replace("(al)", "al")
