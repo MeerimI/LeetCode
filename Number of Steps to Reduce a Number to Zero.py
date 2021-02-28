e4794b4e1c2e423286f3f47df22c7d2e898a7f39
mery <merryoff@gmail.com>
2021-02-28 11:24:15 +0600

    1342. Number of Steps to Reduce a Number to Zero

--- Number of Steps to Reduce a Number to Zero.py
+++ Number of Steps to Reduce a Number to Zero.py
@@ -1,4 +1,6 @@
 """
+case a:
+
 Runtime: 20 ms, faster than 46.72% of Python online submissions for Number of Steps to Reduce a Number to Zero.
 Memory Usage: 13.3 MB, less than 91.94% of Python online submissions for Number of Steps to Reduce a Number to Zero.
 
@@ -18,3 +20,47 @@ class Solution(object):
             else: return 0
         return result
 
+"""
+case b:
+
+Runtime: 24 ms, faster than 19.69% of Python online submissions for Number of Steps to Reduce a Number to Zero.
+Memory Usage: 13.6 MB, less than 12.87% of Python online submissions for Number of Steps to Reduce a Number to Zero.
+
+"""
+
+
+class Solution(object):
+    def numberOfSteps (self, num):
+                               
+        result = 0
+        while num > 0:
+            if num % 2 == 0:
+                num = num / 2
+            elif num % 2 == 1:
+                num -= 1
+            result += 1
+        return result
+
+
+
+"""
+case c:
+
+Runtime: 16 ms, faster than 76.09% of Python online submissions for Number of Steps to Reduce a Number to Zero.
+Memory Usage: 13.3 MB, less than 91.94% of Python online submissions for Number of Steps to Reduce a Number to Zero.
+
+"""
+
+
+class Solution(object):
+    def numberOfSteps (self, num):
+                               
+        result = 0
+        while num > 0:
+            if num % 2 == 0:
+                num = num / 2
+                result += 1
+            elif num % 2 == 1:
+                num -= 1
+                result += 1
+        return result
