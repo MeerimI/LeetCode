501cc4b328ff16113b6a926a90aca694155e485c
MeerimI <irismamatovameerim@gmail.com>
2021-04-20 11:17:48 +0600

    added another solution od

--- 35e.py
+++ 35e.py
@@ -39,3 +39,29 @@ class Solution2(object):
      
             
             
+'''
+Runtime: 24 ms, faster than 99.65% of Python online submissions for Search Insert Position.
+Memory Usage: 14.1 MB, less than 54.08% of Python online submissions for Search Insert Position.
+'''
+
+class Solution3(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+                        
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= target:
+                r = mid - 1
+            elif nums[mid] <= target:
+                l = mid + 1
+        return l            
+                
