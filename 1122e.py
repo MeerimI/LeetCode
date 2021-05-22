73c69ac132f5b36e898258904d35b134af483635
MeerimI <irismamatovameerim@gmail.com>
2021-05-22 11:07:14 +0600

    added another solution of 1122

--- 1122e.py
+++ 1122e.py
@@ -20,3 +20,19 @@ class Solution(object):
                 
         return res + sorted(arr1)
             
+
+'''
+case b:
+
+Runtime: 20 ms, faster than 92.41% of Python online submissions for Relative Sort Array.
+Memory Usage: 13.5 MB, less than 51.22% of Python online submissions for Relative Sort Array.
+
+'''
+
+class Solution2(object):
+    def relativeSortArray(self, arr1, arr2): 
+        arr2_order = {n: i for i, n in enumerate(arr2)}    
+        arr1_tuples = [(arr2_order.get(i, len(arr2_order)), i) for i in arr1]  
+        t = sorted(arr1_tuples)     
+        return [v for k,v in t]    
+        
